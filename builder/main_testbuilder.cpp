f13a88dd4c6b6343dc4cea18ebe412b775ada52e
pitteam11 <117634580+pitteam11@users.noreply.github.com>
2023-02-10 21:20:11 +0300

    Builder

---
+++ builder/main_testbuilder.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "AircraftConstructor.h"
+#include "AircraftBuilder.h"
+#include "HangGliderBuilder.h"
+#include "PassengerPlane.h"
+#include "Aircraft.h"
+
+
+using namespace std;
+
+void main() {
+	try {
+		AircraftBuilder* builder;		
+		AircraftConstructor* constructor =
+			new AircraftConstructor();	
+		
+		builder = new HangGliderBuilder();		
+		constructor->Construct(builder);
+		Aircraft* aircraft = builder->GetAircraft();
+		aircraft->Show();		
+		delete builder;
+
+		builder = new PassengerPlane();
+		constructor->Construct(builder);
+		aircraft = builder->GetAircraft();
+		aircraft->Show();
+
+		delete builder;		
+		delete constructor;		
+	}
+	catch (...) {
+		cout << "Error" << endl;
+	}
+}
